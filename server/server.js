c987bc95829a1778ee58bf66047fd3cdae35d99e
Shagun jain <greatshagun2005@gmail.com>
2025-10-25 22:06:26 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -41,6 +41,8 @@ app.use("/api/auth", userRouter)
 app.use("/api/messages", messageRouter)
 
 await connectDB();
-
-const PORT = process.env.PORT || 5000;
-server.listen(PORT, () => console.log("Server is running on PORT : " + PORT));
+if(process.env.NODE_ENV !== "production"){
+    const PORT = process.env.PORT || 5000;
+    server.listen(PORT, () => console.log("Server is running on PORT : " + PORT));
+}
+export default server;
