da81d7539cc5bc32d63cb2825049f014f046d144
Shagun jain <greatshagun2005@gmail.com>
2025-10-25 22:29:54 +0530

    Update vite.config.js

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,4 +1,3 @@
-// vite.config.js
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
@@ -6,7 +5,7 @@ export default defineConfig({
   plugins: [react()],
   css: {
     postcss: {
-      plugins: [] // fallback to PostCSS, disables lightningcss
+      plugins: [] // fallback to PostCSS only
     }
   }
 })
