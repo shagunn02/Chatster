fb445e2bc6a4aef8ceed826f74ef8b07a2b2b198
Shagun jain <greatshagun2005@gmail.com>
2025-10-25 22:28:08 +0530

    Update vite.config.js

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,8 +1,12 @@
+// vite.config.js
 import { defineConfig } from 'vite'
-import tailwindcss from '@tailwindcss/vite'
+import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  plugins: [
-    tailwindcss(),
-  ],
+  plugins: [react()],
+  css: {
+    postcss: {
+      plugins: [] // fallback to PostCSS, disables lightningcss
+    }
+  }
 })
